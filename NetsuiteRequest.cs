ba4428cc02ff89ffd77dd9302a1e04cac7479757
patatoa <patatoa@gmail.com>
2024-05-15 15:59:00 -0500

    SuiteQL Support

--- NetsuiteRequest.cs
+++ NetsuiteRequest.cs
@@ -16,6 +16,7 @@ namespace NetsuiteRequest
 		private readonly string _tokenSecret;
 		private readonly string _realm;
 		private readonly string _baseUrl;
+		private readonly string _suiteQlBaseUrl;
 
 		public NetsuiteRequest(string consumerKey, string consumerSecret, string token, string tokenSecret, string realm)
 		{
@@ -25,6 +26,7 @@ namespace NetsuiteRequest
 			_tokenSecret = tokenSecret;
 			_realm = realm;
 			_baseUrl = $"https://{_realm.ToLower().Replace("_", "-")}.suitetalk.api.netsuite.com/services/rest/record/v1";
+			_suiteQlBaseUrl = $"https://{_realm.ToLower().Replace("_", "-")}.suitetalk.api.netsuite.com/services/rest/query/v1/suiteql";
 		}
 		public async Task<JsonDocument> Request(string urlString, HttpMethod httpMethod, JsonObject? body = null)
 		{
@@ -63,6 +65,50 @@ namespace NetsuiteRequest
 				default:
 					throw new Exception("Error: Invalid HTTP Method");
 			}
+
+			if (response.IsSuccessStatusCode)
+			{
+				var jsonString = await response.Content.ReadAsStringAsync();
+				return JsonDocument.Parse(jsonString);
+			}
+			else
+			{
+				throw response.StatusCode switch
+				{
+					HttpStatusCode.NotFound => new Exception("Error: Not Found"),
+					HttpStatusCode.BadRequest => new Exception("Error: Bad Request"),
+					HttpStatusCode.Unauthorized => new Exception("Error: Unauthorized" + response.Content.ReadAsStringAsync().Result),
+					HttpStatusCode.Forbidden => new Exception("Error: Forbidden"),
+					HttpStatusCode.InternalServerError => new Exception("Error: Internal Server Error"),
+					_ => new Exception($"Error: {response.StatusCode}"),
+				};
+			}
+		}
+		public async Task<JsonDocument> SuiteQlRequest(JsonObject body, Nullable<int> limit)
+		{
+			var url = _suiteQlBaseUrl;
+			if (limit != null)
+			{
+				url += $"?limit={limit}";
+			}
+			using HttpClient client = new();
+			var oAuth1HeaderGenerator = new OAuth1HeaderGenerator(_consumerKey, _consumerSecret, _token, _tokenSecret, _realm, HttpMethod.Post, url);
+			var header = oAuth1HeaderGenerator.CreateAuthenticationHeaderValue();
+			Console.WriteLine(header);
+			Console.WriteLine(url);
+			client.DefaultRequestHeaders.Authorization = header;
+			client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+			client.DefaultRequestHeaders.Add("Prefer", "transient");
+
+			HttpResponseMessage response;
+	
+			if (body == null)
+			{
+				throw new Exception("Error: Body is required for POST requests");
+			}
+			
+			response = await client.PostAsync(url, new StringContent(body.ToString(), Encoding.UTF8, "application/json"));
+
 
 			if (response.IsSuccessStatusCode)
 			{
